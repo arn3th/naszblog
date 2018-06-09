6042ff46ec02a4045c517d0f0accb3edf887021e
arn3th <artwis21@gmail.com>
2018-06-09 17:42:38 +0200

    zmiana debug

--- naszblog/settings.py
+++ naszblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4#_-2g2odf^)6nx5tq4(^3!n=6x!gugrd$+4f1qvl0%%5jv^+#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
