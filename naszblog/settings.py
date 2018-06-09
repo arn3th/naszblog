4e9be026004ee295d8f118f852d13140073e88f4
arn3th <artwis21@gmail.com>
2018-06-09 19:03:57 +0200

    staticfiles

--- naszblog/settings.py
+++ naszblog/settings.py
@@ -165,7 +165,7 @@ if os.getcwd() == '/app':
     
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
     
-    ALLOWED_HOSTS = ['naszblog-2k18.herokuapp.com']
+    ALLOWED_HOSTS = ['*']
     
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
